6faee5f19c33f69254445da353e90c75ab095f4a
AngieCastano1634 <1634@holbertonschool.com>
2020-04-29 11:19:02 -0500

    fixing pep8

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 10):
     for j in range(i+1, 10):
         print("{}{}".format(i, j), end="")
         if i != 8 or j != 9:
-            print (", ", end="")
+            print(", ", end="")
         else:
             print("\n", end="")
