9e66f5981523ee932dd3a7fd64ea44108d7a7a9d
AngieCastano1634 <1634@holbertonschool.com>
2020-04-29 11:51:06 -0500

    adding index negative case

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    copy = str[: n] + str[n+1:]
+    if n > -1:
+        copy = str[: n] + str[n+1:]
+    else:
+        copy = str
     return (copy)
