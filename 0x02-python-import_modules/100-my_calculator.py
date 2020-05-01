e19e72277eecfe2f3c2d07b6aa16bb127dc3ad67
AngieCastano1634 <1634@holbertonschool.com>
2020-04-30 22:03:06 -0500

    adding the whole phrase

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,16 +2,19 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
+    a = sys.argv[1]
+    b = sys.argv[3]
     if len(sys.argv) != 4:
         print("print Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     if sys.argv[2] == '+':
-        print(add(int(sys.argv[1]), int(sys.argv[3])))
+        print("{} + {} = {}".format(a, b, add(a, b)))
     elif sys.argv[2] == '-':
-        print(sub(int(sys.argv[1]), int(sys.argv[3])))
+        print("{} - {} = {}".format(a, b, sub(a, b)))
     elif sys.argv[2] == "*":
-        print(mul(int(sys.argv[1]), int(sys.argv[3])))
+        print("{} * {} = {}".format(a, b, mul(a, b)))
     elif sys.argv[2] == '/':
-        print(div(int(sys.argv[1]), int(sys.argv[3])))
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
-        print("<a> <operator> <b> = <result>")
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
