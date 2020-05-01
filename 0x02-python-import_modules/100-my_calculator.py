e95264f28c9cb7ab46bd4b1a9c81e63bae5100cb
AngieCastano1634 <1634@holbertonschool.com>
2020-04-30 21:55:14 -0500

    ading elif cases

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
         exit(1)
     if sys.argv[2] == '+':
         print(add(int(sys.argv[1]), int(sys.argv[3])))
-    if sys.argv[2] == '-':
+    elif sys.argv[2] == '-':
         print(sub(int(sys.argv[1]), int(sys.argv[3])))
-    if sys.argv[2] == "*":
+    elif sys.argv[2] == "*":
         print(mul(int(sys.argv[1]), int(sys.argv[3])))
-    if sys.argv[2] == '/':
+    elif sys.argv[2] == '/':
         print(div(int(sys.argv[1]), int(sys.argv[3])))
     else:
         print("<a> <operator> <b> = <result>")
