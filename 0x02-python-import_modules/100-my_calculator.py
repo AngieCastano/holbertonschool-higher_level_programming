330472fcb16cb0c132dc44998747889dc341c900
AngieCastano1634 <1634@holbertonschool.com>
2020-04-30 22:18:14 -0500

    changing to double quotes

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
         exit(1)
     a = sys.argv[1]
     b = sys.argv[3]
-    if sys.argv[2] == '+':
+    if sys.argv[2] == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif sys.argv[2] == '-':
+    elif sys.argv[2] == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
     elif sys.argv[2] == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif sys.argv[2] == '/':
+    elif sys.argv[2] == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
