29d1aff1a7960b3c545518d3c5d84923d3c5895c
AngieCastano1634 <1634@holbertonschool.com>
2020-05-01 00:21:09 -0500

    handling plural and singular

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,10 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
         print("{} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print("{} argument.".format(len(sys.argv) - 1))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv)):
+    for i in range(1, len(sys.argv)):
         if i != 0:
             print("{}: {}".format(i, sys.argv[i]))
