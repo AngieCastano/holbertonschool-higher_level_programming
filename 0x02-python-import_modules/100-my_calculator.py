620dec4c5fc6971f73fdd9aa53c86ccd82f75a55
AngieCastano1634 <1634@holbertonschool.com>
2020-04-30 21:50:21 -0500

    adding every case

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,14 +2,16 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-#    if len(sys.argv) != 4:
-#        print("print Usage: ./100-my_calculator.py <a> <operator> <b>")
-#        exit(1)
-#    if sys.argv[2] == '+':
-#       print(add(int(sys.argv[1]), int(sys.argv[3])))
-#    if sys.argv[2] == '-':
-#        print(sub(int(sys.argv[1]), int(sys.argv[3])))
+    if len(sys.argv) != 4:
+        print("print Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if sys.argv[2] == '+':
+        print(add(int(sys.argv[1]), int(sys.argv[3])))
+    if sys.argv[2] == '-':
+        print(sub(int(sys.argv[1]), int(sys.argv[3])))
     if sys.argv[2] == "*":
         print(mul(int(sys.argv[1]), int(sys.argv[3])))
-#    if sys.argv[2] == '/':
-#        print(div(int(sys.argv[1]), int(sys.argv[3])))
+    if sys.argv[2] == '/':
+        print(div(int(sys.argv[1]), int(sys.argv[3])))
+    else:
+        print("<a> <operator> <b> = <result>")
