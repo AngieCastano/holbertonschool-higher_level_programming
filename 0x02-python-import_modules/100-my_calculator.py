ca8215fb24a6b1e426710f13f02582f717958476
AngieCastano1634 <1634@holbertonschool.com>
2020-04-30 22:15:30 -0500

    changing usage

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,11 +2,11 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
     a = sys.argv[1]
     b = sys.argv[3]
-    if len(sys.argv) != 4:
-        print("print Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
     if sys.argv[2] == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif sys.argv[2] == '-':
