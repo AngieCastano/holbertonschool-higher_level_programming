407321732f80c222a673cfee4e6f317f535cd20c
AngieCastano1634 <1634@holbertonschool.com>
2020-05-05 16:50:19 -0500

    [ fixing spaces.]

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                print("{}".format(matrix[i][j]), end="")
-            print("")
+                if j != len(matrix[i]) - 1:
+                    print("{}".format(matrix[i][j]), end=" ")
+                else:
+                    print("{}".format(matrix[i][j]))
