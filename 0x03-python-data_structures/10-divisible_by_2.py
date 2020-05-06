0ad52a453ccd3ed20f2bdff75efa7a426e40f36b
AngieCastano1634 <1634@holbertonschool.com>
2020-05-05 19:42:50 -0500

    [comprenhesion]

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     if my_list:
-        new = []
-        for i in my_list:
-            new.append(i % 2 - 1)
-        print(new)
-        return new
+        return [ x % 2 == 0 for x in my_list]
