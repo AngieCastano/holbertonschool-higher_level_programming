979a258d04314f11de030ba451a8201daac355fd
AngieCastano1634 <1634@holbertonschool.com>
2020-05-05 19:37:29 -0500

    [unction that finds all multiples of 2 in a list.]

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list:
+        new = []
+        for i in my_list:
+            new.append(i % 2 - 1)
+        print(new)
+        return new
+    else:
+        return None
