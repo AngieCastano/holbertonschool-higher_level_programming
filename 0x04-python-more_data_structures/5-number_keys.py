191b9847b7a0c2a98396ae0899aa82f5fff2ca4c
AngieCastano1634 <1634@holbertonschool.com>
2020-05-07 16:58:08 -0500

    [fixing empty case.]

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    for i, v in enumerate(list(a_dictionary.keys())):
-        pass
-    return i + 1
+    if a_dictionary:
+        for i, v in enumerate(list(a_dictionary.keys())):
+            pass
+        return i + 1
+    else:
+        return 0
