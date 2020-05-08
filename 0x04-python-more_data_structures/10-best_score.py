629c68b15faf2545f5d25ea1ff1ae5a2355b7792
AngieCastano1634 <1634@holbertonschool.com>
2020-05-07 21:35:14 -0500

    [function that returns a key with the biggest integer value.][not importing, using max()]

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    from functools import reduce as f
     if a_dictionary:
-        return f(lambda a, b: a if (a > b) else b, a_dictionary.values())
+        return max(a_dictionary.values())
     return None
