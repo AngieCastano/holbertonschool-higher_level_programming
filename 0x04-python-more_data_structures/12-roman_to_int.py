65e1bb7313ee1cef7dcf11a9abcbe59943c8110d
AngieCastano1634 <1634@holbertonschool.com>
2020-05-07 23:24:33 -0500

    [converts a Roman numeral to an integer.]

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    from functools import reduce
+    if roman_string:
+        n = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        to_sum = [n[letter] for letter in roman_string]
+        return reduce(lambda x, y: x+y, to_sum)
+    return 0
