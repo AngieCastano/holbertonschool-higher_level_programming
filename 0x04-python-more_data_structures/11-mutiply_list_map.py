ef8b7cdbb1b41cb786ab59f70c0f5a770a6489a1
AngieCastano1634 <1634@holbertonschool.com>
2020-05-07 22:22:41 -0500

    [function that returns a list with all values multiplied by a number without using any loops.][using lambda]

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    def mul(my_list):
-        return my_list * number
-    return list(map(mul, my_list))
+    if mutiply_list_map:
+        return list(map(lambda x: x * number, my_list))
+    return None
