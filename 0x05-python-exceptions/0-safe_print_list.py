ff03d7bd0473077246ac3d4c2526393e0e776924
AngieCastano1634 <1634@holbertonschool.com>
2020-05-18 16:19:59 -0500

    [no using if statements, trying to acces ti my_list[iter]]

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    iter = 0
-    for item in my_list:
-        if iter >= x:
-            break
+    printing = -1
+    for iter in range(x):
+        printing = printing + 1
         try:
-            item
+            print("{}".format(my_list[iter]), end="")
         except:
-            pass
-        print("{}".format(item), end="")
-        iter = iter + 1
+            printing = printing - 1
     print("")
-    return (iter)
+    return (printing + 1)
