ea25350ed603f7048bc40cd2afce8be3a7b3f6e0
AngieCastano1634 <1634@holbertonschool.com>
2020-05-19 21:03:16 -0500

    [Write a class Square that defines a square][returns area]

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" creates a square class with its size
+"""
+
+
+class Square:
+        """class Square beginnig
+        """
+        def __init__(self, size=0):
+            """initialization of Square size.
+            Args:
+            size: the size of the squaresquare size.
+            """
+            if type(size) == int:
+                if size < 0:
+                        raise ValueError('size must be >= 0')
+                else:
+                        self.__size = size
+            else:
+                raise TypeError('size must be an integer')
+
+        def area(self):
+            """initialization of Square size.
+            Args:
+            size: the size of the squaresquare size.
+            """
+            return (self.__size ** 2)
