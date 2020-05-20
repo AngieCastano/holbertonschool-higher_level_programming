4dc9b3bc05e881e46f20c0213ca05cf4ff28426f
AngieCastano1634 <1634@holbertonschool.com>
2020-05-19 20:05:59 -0500

    [Class square with its size]

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" creates a square class with its size
+"""
+
+
+class Square:
+        """class Square beginnig
+        """
+        def __init__(self, size):
+            """initialization of Square size.
+            Args:
+            size: the size of the squaresquare size.
+            """
+            self.__size = size
