b5415229895f3b508df494f7b74778b776a0522b
AngieCastano1634 <1634@holbertonschool.com>
2020-05-22 21:49:28 -0500

    [function that adds 2 numbers]

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+"""
+This modul adds 2 integers or floats and returns an int as result
+
+
+"""
+
+
 def add_integer(a, b=98):
-    """ This function only adds float or integers"""
+    """ This function only adds float or integers
+    args: a: first number to add, b: second number. types(int, float)
+    """
     types = [int, float]
-    try:
-        if type(a) not in types:
-            raise TypeError
-    except TypeError:
-        return "a must be an integer"
-    try:
-        if type(b) not in types:
-            raise TypeError
-    except TypeError:
-        return "b must be an integer"
-    else:
-        return int(a) + int(b)
+    if type(a) not in types:
+        raise TypeError("a must be an integer")
+
+    if type(b) not in types:
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
