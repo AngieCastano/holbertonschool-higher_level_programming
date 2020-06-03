2b11692de0e5a3dfbffb789c3d63c963115e2175
AngieCastano1634 <1634@holbertonschool.com>
2020-06-03 17:15:16 -0500

    [comments]

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+ function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
 def read_file(filename=""):
+    """
+    args: filename =  name of the file to read
+    """
     with open(filename, encoding="utf-8") as myFile:
         print(myFile.read(), end="")
