d08948e9283e9ceacf9c354d47f7b573882c8363
AngieCastano1634 <1634@holbertonschool.com>
2020-06-09 13:19:16 -0500

    [tests]

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import unittest
+from models.base import Base
+
+"""
+Test class
+"""
+
+
+class TestBase(unittest.TestCase):
+    def test_base(self):
+        """
+        Test base
+        """
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        b4 = Base(3)
+        b5 = Base()
+        b6 = Base(1000000)
+        b6.id = 9999999
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, 3)
+        self.assertEqual(b5, 0)
+        self.assertEqual(b6.id, 9999999)
