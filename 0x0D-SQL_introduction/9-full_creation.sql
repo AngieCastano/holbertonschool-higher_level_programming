c36cc4a4f2310897504ca1a8da1bf2f7b3f0f945
AngieCastano1634 <1634@holbertonschool.com>
2020-07-13 21:57:45 -0500

    [task 9]

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- creates multiple rows
 -- in a table
-CREATE TABLE second_table IF NOT EXISTS (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table VALUES(1, 'John', 10);
 INSERT INTO second_table VALUES(2, 'Alex', 3);
 INSERT INTO second_table VALUES(3, 'Bob', 14);
