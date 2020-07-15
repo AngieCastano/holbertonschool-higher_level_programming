8eb7057944d9ecd64bb8f3a2c4883863facf7536
AngieCastano1634 <60395533+AngieCastano1634@users.noreply.github.com>
2020-07-14 20:15:17 -0500

    adding where is the reference

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS cities (
      id INT NOT NULL AUTO_INCREMENT UNIQUE,
      name VARCHAR(256) NOT NULL,
      state_id INT NOT NULL,
-     FOREIGN KEY (state_id) REFERENCES states(id),
+     FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
      PRIMARY KEY (id)
 );
