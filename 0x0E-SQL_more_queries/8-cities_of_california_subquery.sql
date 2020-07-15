b06b9f0928598a4bf804fe01c5e0dffef2bc39c3
AngieCastano1634 <60395533+AngieCastano1634@users.noreply.github.com>
2020-07-14 23:22:05 -0500

    again

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- nested queries
 -- get information from another table
 SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id = (
-  SELECT id FROM hbtn_0d_usa.states
+ SELECT id FROM hbtn_0d_usa.states
   WHERE name = 'California'
 )ORDER BY cities.id;
