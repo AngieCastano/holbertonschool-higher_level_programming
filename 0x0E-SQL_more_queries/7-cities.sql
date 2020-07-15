90e60657088c37c075a70ecba9d645b42d7ff509
AngieCastano1634 <60395533+AngieCastano1634@users.noreply.github.com>
2020-07-14 20:18:28 -0500

    not using use

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,7 @@
 -- table
 -- with foreign key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
      id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
      name VARCHAR(256) NOT NULL,
      state_id INT NOT NULL,
