77825217a4771e6af45f5c6c027a169989ee05ec
AngieCastano1634 <1634@holbertonschool.com>
2020-08-08 11:50:04 -0500

    [FIX][compilling]

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
         port=3306
     )
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states WHERE name=% (name)s ORDER BY id ASC",
-                                      {'name': sys.argv[4]})
+    cursor.execute("SELECT * FROM states WHERE name=%(name)s ORDER BY id ASC",
+                   {'name': sys.argv[4]})
     result = cursor.fetchall()
     for row in result:
         print(row)
