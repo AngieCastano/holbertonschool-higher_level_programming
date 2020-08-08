bc74c2dac29669bfbed1654e09219758872b44a0
AngieCastano1634 <1634@holbertonschool.com>
2020-08-08 11:35:44 -0500

    [FIX][closes]

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa"""
-import MySQLdb
+'''
+lists all states from the database hbtn_0e_0_usa
+'''
 import sys
+import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
+
+if __name__ == '__main__':
+    connection = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
         host='localhost',
         port=3306
     )
-    cursor = db.cursor()
+    cursor = connection.cursor()
     cursor.execute("SELECT * FROM states WHERE name=% (name)s ORDER BY id ASC",
                    {'name': sys.argv[4]})
     result = cursor.fetchall()
-    print (result)
+    for row in result:
+        print(row)
+    cursor.close()
+    connection.close()
