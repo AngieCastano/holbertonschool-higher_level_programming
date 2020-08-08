ecd571a26a1bb8ac3f551fb779b145ac21725173
AngieCastano1634 <60395533+AngieCastano1634@users.noreply.github.com>
2020-08-08 11:13:13 -0500

    [FIX][trying the same but different]

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,13 +7,14 @@ import MySQLdb
 
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(
+    c = MySQLdb.connect(
         host="localhost", port=3306, user=sys.argv[1],
         passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    result = cursor.fetchall()
-    for i in result:
-        print(i)
-    cursor.close()
-    db.close() 
+    cu = c.cursor()
+    cu.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cu.fetchall()
+    for row in rows:
+        print(row)
+    cu.close()
+    c.close()
+    
