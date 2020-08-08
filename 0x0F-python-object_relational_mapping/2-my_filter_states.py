efcfad74bf4314a154b9eec257650dd46225ae42
AngieCastano1634 <1634@holbertonschool.com>
2020-08-08 11:38:06 -0500

    [TRY][sames as #1]

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,8 +15,7 @@ if __name__ == '__main__':
         port=3306
     )
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states WHERE name=% (name)s ORDER BY id ASC",
-                   {'name': sys.argv[4]})
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     result = cursor.fetchall()
     for row in result:
         print(row)
