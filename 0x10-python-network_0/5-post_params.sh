eb14c3e1c7fbe26af4018474798109e31a99b748
AngieCastano1634 <1634@holbertonschool.com>
2020-08-18 23:00:37 -0500

    [--data-binary]

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  display allowed methods
-curl -s "$@" -X POST -d "email: hr@holbertonschool.com&subject: I will always be here for PLD"
+curl -s "$@" -X POST --data-binary "email: hr@holbertonschool.com&subject: I will always be here for PLD"
