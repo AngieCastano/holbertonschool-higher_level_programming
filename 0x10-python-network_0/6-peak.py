c031fb90e64217e5e103cac12eaf62c05c5b575f
AngieCastano1634 <1634@holbertonschool.com>
2020-08-18 23:24:58 -0500

    [task #6]

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Sorts integers"""
+
+
+def find_peak(list_of_integers):
+    """Sorts integers"""
+    new = list_of_integers[:]
+    maximo = max(new, default=None)
+    return (maximo)
