d2321b367e8d459f9bf0c90420148033b564009c
AngieCastano1634 <1634@holbertonschool.com>
2020-08-18 22:50:44 -0500

    [square brackets]

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  display allowed methods
-curl -s "$@" -X POST -H "email: hr@holbertonschool.com", "subject: I will always be here for PLD"
+curl -s "$@" -X POST -H ["email: hr@holbertonschool.com", "subject: I will always be here for PLD"]
