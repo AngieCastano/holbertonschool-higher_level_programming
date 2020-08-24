a82c020fd2e484ccec376f661c294e86d221322e
AngieCastano1634 <1634@holbertonschool.com>
2020-08-24 18:09:44 -0500

    [ADD][not executable when imported]

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,9 +2,10 @@
 """
 Getting information about the holberton page
 """
-import urllib.request
-import sys
 
+if __name__ == '__main__':
+    import urllib.request
+    import sys
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers.get("X-Request-Id"))
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get("X-Request-Id"))
