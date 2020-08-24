a72264810e6fac3f7b86a516179d1cf20fdb1975
AngieCastano1634 <1634@holbertonschool.com>
2020-08-24 16:40:35 -0500

    [ADD][iterating last result]

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-
+"""
+Getting information about the holberton page
+"""
 import urllib.request
 
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
     print("Body response:")
-    print("\t-",end="")
-    print(html)
+    for key in html:
+        print("\t-{}".format(key))
