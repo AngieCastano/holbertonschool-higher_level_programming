aa44e8669ad171e1d99d0aef913bdfc33ade3066
AngieCastano1634 <1634@holbertonschool.com>
2020-09-21 19:47:07 -0500

    [UPDATE][Not var but let]

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,8 +8,8 @@ class Rectangle {
   }
 
   print () {
-    for (var j = 0; j < this.height; j++) {
-      for (var i = 0; i < this.width; i++) {
+    for (let j = 0; j < this.height; j++) {
+      for (let i = 0; i < this.width; i++) {
         process.stdout.write('x');
       }
       process.stdout.write('\n');
