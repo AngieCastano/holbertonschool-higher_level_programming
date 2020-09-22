4c0b8b76a71b2dff28b7cb4da7afde94da09ed47
AngieCastano1634 <1634@holbertonschool.com>
2020-09-21 19:42:58 -0500

    [ADD][Printing rectangle]

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (var j = 0; j < this.height; j++) {
+      for (var i = 0; i < this.width; i++) {
+        process.stdout.write("x");
+      }
+      process.stdout.write("\n");
+    }
+  }
+}
+module.exports = Rectangle;
