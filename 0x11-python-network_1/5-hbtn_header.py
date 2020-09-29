941041c36c5d8ac6af7c6dfacb2be0b9c8a6dccf
AngieCastano1634 <1634@holbertonschool.com>
2020-09-29 16:39:50 -0500

    [ADD][X-Request-Id]

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Getting information about the holberton page with request
+"""
+
+if __name__ == '__main__':
+        import requests
+        import sys
+
+        url = sys.argv[1]
+        x = requests.get(url)
+        print(x.headers['X-Request-Id'])
