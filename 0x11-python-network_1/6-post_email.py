e500d3ed4d9eebd701489074fc7e553d05b214b7
AngieCastano1634 <1634@holbertonschool.com>
2020-09-29 17:03:47 -0500

    [UPDATE][import change different place]

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,11 +2,11 @@
 """
 Posting information to an url
 """
+import requests
+import sys
+
 
 if __name__ == '__main__':
-    import requests
-    import sys
-
     url = sys.argv[1]
     x = requests.post(url, data={'email': sys.argv[2]})
     print(x.text)
