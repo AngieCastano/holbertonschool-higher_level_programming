3800eb991b708f29c3293cf130ad67fd9417c3aa
AngieCastano1634 <1634@holbertonschool.com>
2020-09-29 18:19:37 -0500

    [CHANGE][logic]

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-'''
-Post to http://0.0.0.0:5000/search_user API
-'''
+"""
+Post to http://0.0.0.0:5000/search_user
+"""
 import requests
 import sys
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print('No result')
-        exit()
     if len(sys.argv) == 2:
         q = sys.argv[1]
+
         url = 'http://0.0.0.0:5000/search_user'
+
         r = requests.post(url, data={'q': q})
+
         try:
-            json = r.json()
-            print("[{}] {}".format(json.get('id'),
-                                   json.get('name')))
-        except ValueError:
+            response = r.json()
+            print("[{}] {}".format(response['id'], response['name']))
+        except Exception:
             print('Not a valid JSON')
+
+    elif len(sys.argv) == 1:
+        print('No result')
