dade655327be483d2cc858e523b841656b732bbd
AngieCastano1634 <1634@holbertonschool.com>
2020-09-29 19:30:00 -0500

    [ADD][Repo commits]

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Github API
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    rep_n = sys.argv[1]
+    owner_n = sys.argv[2]
+    u = requests.get(f'https://api.github.com/repos/{owner_n}/{rep_n}/commits')
+    dictionary = u.json()
+    for item in dictionary[0:10]:
+        print(item.get('sha'), item.get('author').get('login'))
